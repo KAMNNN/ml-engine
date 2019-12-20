924bea82259262deddd91656c062f6840ead4d22
Monish <monish.ramadoss@outlook.com>
2019-12-20 12:50:35 -0800

    bug fixes and trainging script for bert qgen

--- Transfomer_QANet/data.py
+++ Transfomer_QANet/data.py
@@ -2,7 +2,6 @@ import os
 import json
 import torch
 import urllib.request
-import pandas as pd
 import numpy as np
 import torch.nn as nn
 import subprocess
@@ -10,8 +9,7 @@ import spacy
 from tqdm import tqdm
 from collections import defaultdict
 from transformers import *
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+
 #subprocess.call("python -m spacy download en_core_web_lg")
 nlp = spacy.load("en_core_web_lg")
 
@@ -26,6 +24,7 @@ DEV_SET = {
     "quac" :  "https://s3.amazonaws.com/my89public/quac/val_v0.2.json"
 }
 
+
 SQUAD_TRAIN = "./data/squad-train-v2.0.json"
 SQUAD_DEV   = "./data/squad-dev-v2.0.json"
 COQA_TRAIN = "./data/coqa-train-v1.0.json"
@@ -34,7 +33,7 @@ QUAC_TRAIN = "./data/quac-train-v0.2.json"
 QUAC_DEV   = "./data/quac-dev-v0.2.json"
 
 if not os.path.exists('./data'):
-        os.makedirs('./data')
+    os.makedirs('./data')
 
 def Softmax(x):
     e_x = np.exp(x - np.max(x))
@@ -54,51 +53,71 @@ if not os.path.exists(QUAC_TRAIN):
 if not os.path.exists(QUAC_DEV):
     urllib.request.urlretrieve(DEV_SET['quac'], QUAC_DEV)
 
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
-class DataClass:
-    def __init__ (self, is_train=True):
-        if(is_train):
-            self.data = train()
-        else:
-            self.data = evaluate()        
+class DataClass: 
+    def __init__ (self):
+        self.data = _PreProcess()       
     def __len__(self):
         return len(self.data)
     def __getitem__(self, idx):
-        data = self.data[idx]     
-        c,q,a,s = data['context'], data['question'], data['answer'], data['spans']
+        t = self.data[idx]     
+        c,q,a,s = t['text']['context'], t['text']['question'], t['text']['answer'], t['sentence']
 
+        input_str = "[CLS]" + s + "[SEP]" + a + "[SEP]" 
+        input_tokens = tokenizer.encode(input_str)
+        mask_tokens = tokenizer.encode("[MASK]")  
+        output_tokens = tokenizer.encode(q[:-1])
+
+        input_tensor = torch.tensor(input_tokens).unsqueeze(0)
+        masks_tokens_tensor = torch.tensor(mask_tokens).unsqueeze(0)
+        output_tensor = torch.tensor(output_tokens).unsqueeze(0)
         
-def train():
-    squad_df =  pd.read_json(SQUAD_TRAIN)['data']
-    coqa_df =   pd.read_json(COQA_TRAIN)['data']
-    quac_df =   pd.read_json(QUAC_TRAIN)['data']
-    L1 = _preprocess_squad(squad_df)
-    L2 = _preprocess_coqa(coqa_df)
-    L3 = _preporcess_quac(quac_df)
-    return pd.DataFrame(L1 + L2 + L3)
+        return input_tensor, output_tensor, masks_tokens_tensor
 
-def evaluate():
-    squad_df =  pd.read_json(SQUAD_DEV, encoding='utf-8')['data']
-    coqa_df =   pd.read_json(COQA_DEV)['data']
-    quac_df =   pd.read_json(QUAC_DEV)['data']
-    L1 = _preprocess_squad(squad_df, False)
-    L2 = _preprocess_coqa(coqa_df, False)
-    L3 = _preporcess_quac(quac_df, False)
-    return pd.DataFrame(L1 + L2 + L3)
-   
+def _PreProcess():   
+    s = [json.load(open(SQUAD_TRAIN)), json.load(open(SQUAD_DEV))]
+    c = [json.load(open(COQA_TRAIN)), json.load(open(COQA_DEV))]
+    q = [json.load(open(QUAC_TRAIN)), json.load(open(QUAC_DEV))]
+    squad = defaultdict(list)
+    coqa  = defaultdict(list)
+    quac  = defaultdict(list)
+
+    for d in s:
+        for k, v in d.items():
+            squad[k] += v
+    for d in c:
+        for k, v in d.items():
+            coqa[k] += v
+    for d in q:
+        for k, v in d.items():
+            quac[k] += v
+
+    L1 = _preprocess_squad(squad['data'])
+    L2 = _preprocess_coqa(coqa['data'])
+    #L3 = _preporcess_quac(quac['data'])
+
+    return L1 + L2 #+ L3
 
 def _get_answer_spans(para_text):
     para_nlp = nlp(para_text)
     sentences = [(x.text, x.start_char) for x in para_nlp.sents]
     entities, entity_dict = [], {}
-    
-    for x in para_nlp.ents:
-        
+
+    sentence_lengths = [len(sentences[0][0])]
+    for s in range(1, len(sentences)):
+        sentence_lengths.append(sentence_lengths[s-1]+len(sentences[s][0]))        
+    for x in para_nlp.ents:      
         if x.text in entity_dict:
             continue
         entity_dict[x.text] = 1
-        entities.append((x.text, x.start_char, x.end_char))
-
+        for i in range(len(sentence_lengths)):
+            if(x.start_char < sentence_lengths[i]):
+                sent = sentences[i]
+                break
+            else:
+                sent = sentences[0]
+        entities.append((x.text, sent))
     return entities
 
 def _neural_get_answer_spans(para_text, _processed_spans=[]):
@@ -116,8 +135,7 @@ def _neural_get_answer_spans(para_text, _processed_spans=[]):
                     sent = sentences[i]
                     break
                 else:
-                    sent = sentences[0]   
-            
+                    sent = sentences[0]               
             spans.append(span.lower())
             input_ids = tokenizer.encode("[CLS]") + tokenizer.encode(sent) + tokenizer.encode("[SEP]")
             token_type_ids = [0 if i <= input_ids.index(102) else 1 for i in range(len(input_ids))]
@@ -125,62 +143,83 @@ def _neural_get_answer_spans(para_text, _processed_spans=[]):
             all_tokens = tokenizer.convert_ids_to_tokens(input_ids)
             bert_span = ' '.join(all_tokens[torch.argmax(start_scores) : torch.argmax(end_scores)+1]).lower()
             if(bert_span not in spans and bert_span != ''):
-                spans.append(bert_span)
+                spans.append((bert_span, sent))     
     return spans
 
-def _cleanup(context, question, answer):
-    context_doc = nlp(context)
-    question_doc = nlp(question)
-    answer_doc = nlp(answer)     
+def _cleanup(context, question, answer, start):
+    def clean_text(text):
+        text = text.replace("]", " ] ")
+        text = text.replace("[", " [ ")
+        text = text.replace("\n", " ")
+        text = text.replace("''", '" ').replace("``", '" ')
+        return text
+
+    context_doc = nlp(clean_text(context))
+    question_doc = nlp(clean_text(question))
+    answer_doc = nlp(clean_text(answer))
     
     output_context  = defaultdict(list)
     output_question = defaultdict(list)
     output_answer =   defaultdict(list)
-   
+
+    sentences = [str(sents) for sents in context_doc.sents]
+    sent_lengths = [len(sentences[0])]
+    for i in range(1, len(sentences)):
+        sent_lengths.append(len(sentences[i]) + sent_lengths[i-1])
+    for i in range(len(sent_lengths)):
+                if(start < sent_lengths[i]):
+                    sent = sentences[i]
+                    break
+                else:
+                    sent = sentences[0]
+
     for token in context_doc:
-        output_context['text'].append(token.text)
         output_context['pos'].append(token.pos)
-    for ent in context_doc.ents:
-        output_context['ent'].append(ent.label)
-        #print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)
+    for ent in context_doc.ents:  
+        txt = ent.text.split(' ')    
+        output_context['ner'].append(ent.label)
     for token in question_doc:
-        output_question['text'].append(token.text)
         output_question['pos'].append(token.pos)
     for ent in question_doc.ents:
+        txt = ent.text.split(' ')
         output_question['ner'].append(ent.label)
-        #print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)
     for token in answer_doc:
-        output_answer['text'].append(token.text)
         output_answer['pos'].append(token.pos)
     for ent in answer_doc.ents:
+        txt = ent.text.split(' ')
         output_answer['ner'].append(ent.label)
-        #print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)
-        
-    spans = _get_answer_spans(context)    
-    #spans = _neural_get_answer_spans(context, spans) 
+       
+    output_context['text'] =    clean_text(context)
+    output_question['text'] =   clean_text(question)
+    output_answer['text'] =     clean_text(answer)
+
+    #spans = _get_answer_spans(clean_text(context)) 
+    #spans = _neural_get_answer_spans(clean_text(context), spans) 
 
     output = {
-        "ner": {  
-            "cotext": output_context['text'],
+        "text": {  
+            "context": output_context['text'],
             "question": output_question['text'],
-            "answer": output_answer['text']
+            "answer": output_answer['text'],
+           #"sentence":
         },
         "pos": { 
-            "cotext": output_context['pos'],
+            "context": output_context['pos'],
             "question": output_question['pos'],
             "answer": output_answer['pos']
         },
         "ner": {
             "context": output_context['ner'],
-            "questions": output_question['ner'],
+            "question": output_question['ner'],
             "answer": output_answer['ner'],
                 },
-        "spans": spans,
+        "sentence": sent,
+        #"spans": spans,
     }
 
     return output
 
-def _preprocess_squad(data, is_training=True):   
+def _preprocess_squad(data):   
     output = list()
     tmp = list()
     for entry in tqdm(data, desc='PreProcess Squad'):
@@ -189,21 +228,21 @@ def _preprocess_squad(data, is_training=True):
             for qa in paragraph["qas"]:
                 qas_id = qa["id"]
                 question = qa["question"]
-                if is_training:
-                    is_impossible = qa["is_impossible"]                    
-                    if not is_impossible:
-                        answer = qa["answers"][0]
-                        orig_answer_text =  answer["text"]
-                        answer_offset =     answer["answer_start"]
-                    else:
-                        orig_answer_text = ""   
-                    tmp.append((context, question, orig_answer_text))
+                is_impossible = qa["is_impossible"]                    
+                if not is_impossible:
+                    answer = qa["answers"][0]
+                    orig_answer_text =  answer["text"]
+                    start =             answer["answer_start"]                  
+                else:
+                    orig_answer_text = ""   
+                tmp.append((context, question, orig_answer_text, start))
 
-    for context, question, orig_naser_test in tqdm(tmp, desc='PreProcess Squad Cleanup'):
-        output.append(_cleanup(context, question, orig_answer_text))
+    for context, question, answer, start in tqdm(tmp, desc='PreProcess Squad Cleanup'):
+        output.append(_cleanup(context, question, answer, start))
+        
     return output
 
-def _preprocess_coqa(data, is_training=True):
+def _preprocess_coqa(data):
     output = list()
     tmp = list()
     for entry in tqdm(data, desc='PreProcessing Coqa'):
@@ -211,17 +250,18 @@ def _preprocess_coqa(data, is_training=True):
         for q, a in zip(entry['questions'], entry['answers']):
             question = q['input_text']
             answer = a['input_text']
-            answer_text = a['span_text']            
+            answer_text = a['span_text']
+            start = a['span_start']
             if(answer == ''):
                 answer = context[ a['span_start']:a['span_end']]    
-            tmp.append((context, question, answer_text))
+            tmp.append((context, question, answer_text, start))
 
-    for context, question, orig_naser_test in tqdm(tmp, desc='PreProcess Coqa Cleanup'):
-        output.append(_cleanup(context, question, orig_answer_text))
-
+    for context, question, answer, start in tqdm(tmp, desc='PreProcess Coqa Cleanup'):
+        output.append(_cleanup(context, question, answer, start))
+        
     return output
 
-def _preporcess_quac(data, is_training=True):
+def _preporcess_quac(data):
     output = list()
     tmp = list()
     for entry in tqdm(data, desc='PreProcessing Quac'):
@@ -232,14 +272,17 @@ def _preporcess_quac(data, is_training=True):
                 question = qa['question']                
                 for a in qa['answers']:
                     answer = a['text']
-                    start_pos = a['answer_start']
+                    start = a['answer_start']
                     followup = qa['followup']
                     yesno = qa['yesno']
-                    tmp.append((context, question, answer_text))
+                    tmp.append((context, question, answer, start))
 
-    for context, question, orig_naser_test in tqdm(tmp, desc='PreProcess Quac Cleanup'):
-        output.append(_cleanup(context, question, orig_answer_text))
+    for context, question, answer, start in tqdm(tmp, desc='PreProcess Quac Cleanup'):
+        output.append(_cleanup(context, question, answer, start))
+        
     return output
 
         
 data = DataClass
+
+
