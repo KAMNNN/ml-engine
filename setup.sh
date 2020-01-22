97a3da3a49d5071e7a55807da20c3508e3f325dd
mramados <monish.ramadoss@outlook.com>
2020-01-21 23:35:29 -0800

    added setup

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export CONDA_ALWAYS_YES="true"
+conda create -n kamn python=3.7
+conda install -n kamn anaconda
+conda install -n kamn pytorch cpuonly -c pytorch
+conda install -n kamn -c anaconda gensim
+unset CONDA_ALWAYS_YES 
+conda init --all --dry-run --verbose
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+source activate kamn
+pip install transformers 
+git submodule init
+git submodule update
+cd external/neuralcoref
+pip install -r requirements.txt
+pip install -e .
+python -m spacy download en
+cd ../../
